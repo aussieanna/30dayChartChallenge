3b82b12b432b25f8497f0148b58429121e1a19fb
Anna Reynolds <annareynolds01@gmail.com>
2021-04-09 16:02:58 +1000

    Made 2 more charts

--- 10_abstract/10_abstract.R
+++ 10_abstract/10_abstract.R
@@ -6,7 +6,7 @@ library(ggfx)
 dataset <- data.frame(x=rnorm(1000), y=rnorm(1000))
 
 #plot data
-ggplot(dataset, aes(x,y)) +
+p1 <- ggplot(dataset, aes(x,y)) +
   geom_line(colour="purple") + 
   with_blur(geom_point(shape=8, colour="yellow", size=3),
             sigma = unit(0.5, 'mm')
@@ -15,4 +15,35 @@ ggplot(dataset, aes(x,y)) +
   coord_polar() + 
   theme(plot.background = element_rect(fill = "black", color = "black"))
 
-ggsave("10_abstract/10_abstract.png", device = "png", type = "cairo", width = 8, height = 8) #Use device= and type= to stop images being pixelated
+ggsave(p1, "10_abstract/10_abstract.png", plot = p1, device = "png", type = "cairo", width = 8, height = 8) #Use device= and type= to stop images being pixelated
+
+
+dataset2 <- data.frame(x=rnorm(1000), y=rnorm(1000))
+
+#plot data
+p2 <- ggplot(dataset2, aes(x,y)) +
+  geom_path(colour="blue") + 
+  with_blur(geom_point(shape=8, colour="red", size=3),
+            sigma = unit(0.5, 'mm')
+  ) +
+  theme_void() +
+  coord_polar() + 
+  theme(plot.background = element_rect(fill = "black", color = "black"))
+p2
+
+ggsave("10_abstract/10_abstract2.png", plot = p2, device = "png", type = "cairo", width = 8, height = 8) #Use device= and type= to stop images being pixelated
+
+dataset3 <- data.frame(x=rnorm(1000), y=rnorm(1000))
+
+#plot data
+p3 <- ggplot(dataset3, aes(x,y)) +
+  geom_line(colour="turquoise") + 
+  with_blur(geom_point(shape=8, colour="orange", size=3),
+            sigma = unit(0.5, 'mm')
+  ) +
+  theme_void() +
+  coord_polar() + 
+  theme(plot.background = element_rect(fill = "black", color = "black"))
+p3
+
+ggsave("10_abstract/10_abstract3.png", plot = p3, device = "png", type = "cairo", width = 8, height = 8) #Use device= and type= to stop images being pixelated
