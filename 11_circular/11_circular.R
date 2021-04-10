867007fe182d66d5c0d00dd100657da5f059dbbe
Anna Reynolds <annareynolds01@gmail.com>
2021-04-11 08:34:04 +1000

    Tweaked plot and saved as png

--- 11_circular/11_circular.R
+++ 11_circular/11_circular.R
@@ -18,7 +18,8 @@ meantmp <- cbr_2020 %>% group_by(valid2) %>%
 
 tmp_data <- merge(maxtmp, mintmp, c("valid2"))
 tmp_data2 <- merge(tmp_data, meantmp, c("valid2"))
-cbr_2020_2 <- merge(cbr_2020, tmp_data2, c("valid2")) %>% distinct(valid2, min, max, mean)
+cbr_2020_2 <- merge(cbr_2020, tmp_data2, c("valid2")) %>% distinct(valid2, min, max, mean) %>%
+  mutate(valid2 = as.Date(valid2))
 
 cbr_plot <- ggplot(cbr_2020_2, aes(valid2,
                             ymin = min,
@@ -27,13 +28,17 @@ cbr_plot <- ggplot(cbr_2020_2, aes(valid2,
   geom_linerange(size = 1.3, alpha = 0.75) +
   scale_color_viridis_c(NULL, option = "plasma") +
   coord_polar() + 
-  theme_void() +
+  theme_minimal() +
   theme(legend.position = "right") +
-  #scale_x_date(labels = date_format("%b"), breaks = date_breaks("month")) +
-  labs(title = "Canberra Weather Radial",
-       subtitle = "1 Jan to 31 Dec 2020",
-       caption = "Mean temperature",
+  scale_x_date(labels = date_format("%b"), breaks = date_breaks("month")) +
+  labs(title = "Canberra Weather",
+       subtitle = "Mean temperature, 1 Jan to 31 Dec 2020",
+       caption = "Data: Australian Bureau of Meterology ",
        x = NULL, y = NULL) + 
-  ylim(-10, 45)
+  scale_y_continuous(breaks=NULL) +
+  expand_limits(y=0) 
 
 cbr_plot
+
+#Save the most recent chart as an image (inches is default)
+ggsave("11_circular/11_circular.png", device = "png", type = "cairo", width = 6, height = 6) #Use device= and type= to stop images being pixelated
