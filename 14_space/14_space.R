cb395fef2c04af45781c9a974f7902b03fd1fb62
Anna Reynolds <annareynolds01@gmail.com>
2021-04-14 16:40:44 +1000

    Added annotation

--- 14_space/14_space.R
+++ 14_space/14_space.R
@@ -1,3 +1,4 @@
+extrafont::loadfonts(device="win")
 
 library(tidyverse)
 library(jpeg)
@@ -21,15 +22,23 @@ ggplot(iss, aes(x = Year, y = Spacewalks)) +
   geom_bar(stat = "identity", alpha = 0.75, colour = "pink", fill = "pink") +
   xlab("Year") +
   ylab("Spacewalks") +
-  ggtitle("International Space Station Spacewalks") +
+  ggtitle("International Space Station Spacewalks: 1998 to 2021") +
+  labs(caption = "Data: NASA:https://www.nasa.gov/mission_pages/station/spacewalks/") +
   theme_minimal() +
   geom_text(aes(label = Spacewalks), size = 4, fontface = 2, 
             colour = 'white', hjust = 0.5, vjust = 1) +
-  theme(plot.title.position = "plot",
+  theme(plot.title = element_text(
+    family = "Roboto",
+    size = 12,
+    colour = "white",
+    vjust = 1
+  ),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         text = element_text(colour = "white"),
         axis.text = element_text(size = rel(0.8), colour = "white"),
-        plot.background = element_rect(fill = "black", color = "black"))
+        plot.background = element_rect(fill = "black", color = "black")) + 
+  annotate("text", x = 2016, y = 21, label = "There have been 237 spacewalks at the ISS\nsince December 1998", colour = "white")
 
+#Save image as png
 ggsave("14_space/14_space.png", device = "png", type = "cairo", width = 8, height = 6) #Use device= and type= to stop images being pixelated
